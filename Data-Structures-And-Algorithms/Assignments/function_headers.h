4a58d257db97c282751501364956764a6da1202f
farazn019 <naseemf@uwindsor.ca>
2020-06-03 02:15:46 -0400

    Updated the first assignment. Things work better now

---
+++ Data-Structures-And-Algorithms/Assignments/function_headers.h
@@ -0,0 +1,14 @@
+/*
+#include <iostream>
+#include "Node.cpp"
+
+void menu();
+void initalizeList(Node **head);
+int sizeOfList(Node **head);
+void displayAllProducts(Node **head);
+void addNewProduct(Node **head);
+void displayASingleProduct(Node **head, int ID);
+void sortList(Node **head);
+void deleteASingleProduct(Node **head, int ID);
+void deleteAllProducts(Node **head);
+int computeLengthString(std::string Str);*/
